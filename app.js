f27e534ebb56d040bf542384e364ffe8eb24cce9
Wissam Charafeddine <wissam.charafeddine@gmail.com>
2016-04-05 21:25:03 -0400

    initial complete

--- app.js
+++ app.js
@@ -10,16 +10,29 @@ $(document).ready(function(){
     home.fadeTo("slow",1);
   });
   home.on('click', function() {
-    home.css('background-color', 'white');
-    home.after( "<b>clicked</b>");
+    home.css('background-color', 'gray');
+    home.add(" <span><b> clicked</b></span>").appendTo(home)
   });
-  //
-  // var img = $('#myPicture');
-  // console.log(img.attr('src'));
-  // console.log(img.css('width'));
-  // img.css('width','400px');
-  // // img.css('width', '1000px').hide().delay(400).fadeIn(1800);
-  // img.on('click', function(){
-  //   img.hide().delay(400).fadeIn(1800);
-  // })
+
+  about.hover(function(){
+    about.fadeTo('slow',0.25);
+  },
+  function(){
+    about.fadeTo("slow",1);
+  });
+  about.on('click', function() {
+    about.css('background-color', 'white');
+    about.add(" <span><b> clicked</b></span>").appendTo(about)
+  });
+
+  contact.hover(function(){
+    contact.fadeTo('slow',0.25);
+  },
+  function(){
+    contact.fadeTo("slow",1);
+  });
+  contact.on('click', function() {
+    contact.css('background-color', 'white');
+    contact.add(" <span><b> clicked</b></span>").appendTo(contact)
+  });
 });
